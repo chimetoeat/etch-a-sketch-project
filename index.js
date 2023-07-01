68733123a8f5438f2db5bf567802160311a8ddfd
Vince Ivan Minez <banongofficial28@gmail.com>
2023-07-01 21:27:43 +0800

    Completed Grid Functionalities

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-let grid = document.querySelector(".grid");
+let container = document.querySelector(".container")
 
+let btn = document.querySelector("#btn")
 
+let size = prompt("Enter grid size: ");
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-    let box = document.createElement("div")
-    box.classList.add("box")
-    grid.appendChild(box)
+btn.addEventListener('click', () => {
+    location.reload()
+  });
+
+function createGrid(size) {
+    let grid = document.querySelector(".grid");
+    if (!grid) {
+        grid = document.createElement("div")
+        grid.classList.add("grid")
+    }
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div")
+        row.classList.add("row")
+        grid.appendChild(row)
+        for (let j = 0; j < size; j++) {
+            let col = document.createElement("div")
+            col.classList.add("col")
+    
+            col.addEventListener(
+            "mouseover",
+            (event) => {
+                // highlight the mouseover target
+                event.target.style.backgroundColor = "orange";
+            
+            },
+            false
+            );
+    
+            row.appendChild(col)
+        }
+        
     }
 }
+
+createGrid(size)
+
+
+
+
